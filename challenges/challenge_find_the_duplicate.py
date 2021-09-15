54b3a328e1d62a524f3d7573e5f01735d7142465
Ediberto Oliveira <edibertooliveira@aol.com>
2021-09-15 15:59:09 -0300

    feat: requirement 04

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,11 @@
+from typing import Counter
+
+
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    if len(nums) <= 1 or any(type(num) != int or num < 0 for num in nums):
+        return False
+    DUPLICATE_DICT = Counter(nums)
+    if all(value == 1 for value in DUPLICATE_DICT.values()):
+        return False
+    return max(DUPLICATE_DICT, key=DUPLICATE_DICT.get)
