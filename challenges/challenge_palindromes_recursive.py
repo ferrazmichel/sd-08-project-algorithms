708e3cede103640fa10c083efeb19e07bf84509d
caiomzrocha <caiomzrocha@gmail.com>
2021-09-15 19:24:32 -0300

    [REQ-02] Implementa a funçao study_schedule

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,18 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if word == "":
+        return False
+
+    if len(word[low_index:high_index + 1]) == 1:
+        return True
+
+    if (
+        len(word[low_index:high_index + 1]) == 2
+        and word[low_index] == word[high_index]
+    ):
+        return True
+
+    low_and_high_index_is_equal = word[low_index] == word[high_index]
+    return low_and_high_index_is_equal and is_palindrome_recursive(
+        word, low_index + 1, high_index - 1
+    )
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,12 +3,12 @@ def study_schedule(permanence_period, target_time):
     if type(target_time) != int:
         return None
 
-    count = 0
+    target_time_count = 0
     for period in permanence_period:
         if type(period[0]) != int or type(period[1]) != int:
             return None
 
         if period[0] <= target_time <= period[1]:
-            count += 1
+            target_time_count += 1
 
-    return count
+    return target_time_count
