ea039e8251e5915598384e8f58c17ccb6737200a
Daniel <danieldsrd@gmail.com>
2021-09-15 17:41:25 -0300

    Req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,15 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) == len(second_string):
+        if (freq(first_string) == freq(second_string)):
+            return True
+        return False
+    else:
+        return False
+
+
+# https://www.quora.com/How-can-I-compare-two-words-on-Python-3-and-check-if-they-are-anagrams-of-each-other-specifically-without-the-use-of-built-in-functions
+def freq(word):
+    freq_dict = {}
+    for char in word:
+        freq_dict[char] = freq_dict.get(char, 0) + 1
+    return freq_dict
