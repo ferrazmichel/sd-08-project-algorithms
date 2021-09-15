78fbbfb07e46622879a5ce67997b48b9b1d1a270
hugobrr <hugobrr@gmail.com>
2021-09-15 16:45:38 -0300

    Teste 3 requisito com add

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,18 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    counter = 0 
+    if len(nums) <= 2:
+        return False
+    tested_number = nums[0]
+    for number in nums[1:]:
+        try:
+            result = number > 0
+        except TypeError:
+            return False
+        if result is True:
+            return False
+        if tested_number == number:
+            counter += 1
+    if counter == 1:
+        return True
+    else:
+        return False
