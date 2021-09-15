160bcd8527a38e1ca4e6606edd9677329beec8eb
albertassi88 <albertassi88@gmail.com>
2021-09-15 17:52:02 -0300

    Requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,11 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if first_string == '' or second_string == '':
+        return False
+    while len(first_string) != 0:
+        char = first_string[0]
+        first_string = first_string.replace(char, '')
+        second_string = second_string.replace(char, '')
+
+        if len(first_string) != len(second_string):
+            return False
+    return True
