ac3228c8d25845a810da434045292b9c06c54e95
Lucas Nascimento - MACOS <lucasmvnascimento@gmail.com>
2021-09-15 15:18:23 -0300

    req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui.  """
+    """ Faça o código aqui. """
+    if (target_time is None) or (permanence_period is None):
+        return None
+    cont = 0
+    for tuple in permanence_period:
+        if ((type(tuple[0]) != int) or type(tuple[1]) != int):
+            return None
+        if tuple[0] <= target_time <= tuple[1]:
+            cont += 1
+    return cont
