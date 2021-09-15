bc5d7ccef2f2d8acd8877eb75c89d67531023077
Ediberto Oliveira <edibertooliveira@aol.com>
2021-09-15 15:19:29 -0300

    feat: requirement 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    MAX_STUDENTS = 0
+    try:
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                MAX_STUDENTS += 1
+    except TypeError:
+        return None
+    return MAX_STUDENTS
