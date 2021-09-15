36f3d3a28db6b2e274818f4e363dee98ae97cb8f
Fabio Ferreira <fabio35547@gmail.com>
2021-09-15 20:45:00 -0300

    Requisito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,21 @@
+def check_duplicate(nums, num):
+    if nums.count(num) > 1:
+        return True
+    return False
+
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    num_duplicate = 0
+
+    if len(nums) <= 1 or any(isinstance(num, str) or num < 0 for num in nums):
+        return False
+
+    for num in nums:
+        if (check_duplicate(nums, num)):
+            if (num_duplicate > 0 and num_duplicate != num):
+                return False
+            num_duplicate = num
+
+    if (num_duplicate > 0):
+        return num_duplicate
+    return False
