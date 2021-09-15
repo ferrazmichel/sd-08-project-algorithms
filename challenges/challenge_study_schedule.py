9172f52e32eb42939357416d9a29dda35d9b40c5
hugobrr <hugobrr@gmail.com>
2021-09-15 15:08:27 -0300

    Teste 1 requisito

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,17 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    if target_time is not None:
+        for moment in range(len(permanence_period)):
+            if (
+                permanence_period[moment][0] is None
+                or permanence_period[moment][1] is None or
+                not isinstance(permanence_period[moment][0], int) or
+                not isinstance(permanence_period[moment][1], int)
+            ):
+                return None
+            if (
+                target_time >= permanence_period[moment][0]
+                and target_time <= permanence_period[moment][1]
+            ):
+                counter += 1
+        return counter
