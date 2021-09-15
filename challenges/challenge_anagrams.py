c44c6c0cb3e3a3ed21495e65bee24dfb6fced272
hugobrr <hugobrr@gmail.com>
2021-09-15 16:18:09 -0300

    Teste 3 requisito

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,9 @@
+from challenges.smoothsort import smoothsort
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    order_first = smoothsort(list(first_string))
+    order_second = smoothsort(list(second_string))
+    if order_first == order_second:
+        return True
+    return False
