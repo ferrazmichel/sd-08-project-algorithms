32eaab36e4e245c5521a492aece5da73f03e56f2
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2021-09-15 09:29:50 -0300

    Requisito 1, mudanca no arquivo challenge_study_schedule.py

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    if not isinstance(permanence_period, list) or not isinstance(
+        target_time, int
+    ):
+        return None
+    result = 0
+    for number in permanence_period:
+        if not isinstance(number[0], int) or not isinstance(number[1], int):
+            return None
+        if number[0] <= target_time <= number[1]:
+            result += 1
+    return result
