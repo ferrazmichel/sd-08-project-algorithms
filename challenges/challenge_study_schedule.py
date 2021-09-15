f35d8d916478fbe318310aed737095f6f498a79c
caiomzrocha <caiomzrocha@gmail.com>
2021-09-15 18:50:33 -0300

    [REQ-01] Implementa a funçao study_schedule.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    if type(target_time) != int:
+        return None
+
+    count = 0
+    for period in permanence_period:
+        if type(period[0]) != int or type(period[1]) != int:
+            return None
+
+        if period[0] <= target_time <= period[1]:
+            count += 1
+
+    return count
