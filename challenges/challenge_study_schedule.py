2687c23849ab40c84424d7b5c8e50e959bc8067d
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-09-15 20:59:38 -0300

    iniciando projeto e adicionando requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None or target_time < 0 or target_time > 23:
+        return None
+
+    count = 0
+
+    for student in permanence_period:
+        if not isinstance(student[0], int) or not isinstance(student[1], int):
+            return None
+        if student[0] <= target_time <= student[1]:
+            count = count + 1
+
+    return count
+
+
+print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 1))
