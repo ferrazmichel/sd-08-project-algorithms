339371ba1860b796a8ad423f629f2f4ec9a3885d
jnpefo <jnpefo@gmail.com>
2021-09-15 17:16:51 -0300

    Req 1, tenta 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,7 +1,7 @@
 def study_schedule(permanence_period, target_time):
     try:
         count = 0
-        if (target_time == type(None)): return None
+        if not (isinstance(target_time, int)): return None
         for index in permanence_period:
             if (index[0] <= target_time <= index[1]):
                 count += 1
