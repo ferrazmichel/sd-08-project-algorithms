a5d7b34d18e64e02d603b0985e10951135551c76
Leandro Lachini <leandrolachini@gmail.com>
2021-09-15 20:11:16 -0300

    task 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,7 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    # https://www.w3schools.com/python/python_howto_reverse_string.asp
+    if word == '':
+        return False
+
+    return word == word[::-1]
