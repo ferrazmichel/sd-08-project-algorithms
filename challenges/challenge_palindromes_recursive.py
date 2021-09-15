dd7d1e83ceb6d256ac63dcdfa722f0d24a40ced4
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2021-09-15 10:50:34 -0300

    Requisito 2, modificao no arquivo challenge_palindromes_recursive.py

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,20 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+    if low_index == high_index:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    if low_index < high_index + 1:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+
+# Solução encontrada no link:
+# https://www.geeksforgeeks.org/recursive-function-check-string-palindrome/
+# Se houver um único caractere ele retorna True, por isso a comparação entre 
+# low e high;
+# Se a palava no index low for diferente no index high retorna False, pois 
+# eles não correspondem;
+# Se index low for menor que o high ele faz a recursividade aumentado o low
+# e diminuindo o high.
