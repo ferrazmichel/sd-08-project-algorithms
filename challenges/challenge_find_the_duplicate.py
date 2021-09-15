bca782aced5f27ce6acabf26df06e290f73ca2e2
hugobrr <hugobrr@gmail.com>
2021-09-15 17:15:39 -0300

    Teste 5 requisito

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,18 +1,16 @@
 def find_duplicate(nums):
     counter = 0
-    if len(nums) <= 2:
+    size = len(nums)
+    if size <= 2:
         return False
-    tested_number = nums[0]
-    for number in nums[1:]:
-        try:
-            result = number > 0
-        except TypeError:
-            return False
-        if result is True:
-            return False
-        if tested_number == number:
-            counter += 1
+    tested_position = 0
+    while tested_position < size:
+        # tested_number = nums[tested_position]
+        # for number in nums[tested_position + 1]:
+        #     if number > 0 or counter == 2:
+        #         return False
+        #     if tested_number == number:
+        #         counter += 1
+        tested_position += 1
     if counter == 1:
         return True
-    else:
-        return False
--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,9 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    size = len(word)
+    if size < 1:
+        return False
+    for index in range(size // 2):
+        if word[index] != word[size - index - 1]:
+            return False
+    return True
