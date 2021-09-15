fb57f2771328d5887634d028f4f1bb641f0cacb1
Ediberto Oliveira <edibertooliveira@aol.com>
2021-09-15 16:05:32 -0300

    fix: linter

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -6,6 +6,7 @@ def is_anagram(first_string, second_string):
         char = first_string[0]
         first_string = first_string.replace(char, "")
         second_string = second_string.replace(char, "")
+
         if len(first_string) != len(second_string):
             return False
     return True
--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -5,7 +5,9 @@ def find_duplicate(nums):
     """ Faça o código aqui. """
     if len(nums) <= 1 or any(type(num) != int or num < 0 for num in nums):
         return False
+
     DUPLICATE_DICT = Counter(nums)
     if all(value == 1 for value in DUPLICATE_DICT.values()):
         return False
+
     return max(DUPLICATE_DICT, key=DUPLICATE_DICT.get)
