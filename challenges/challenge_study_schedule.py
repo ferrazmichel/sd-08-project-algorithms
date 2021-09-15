068d87a173a56dbbce3cd3a1592e3cd8d067d380
jnpefo <jnpefo@gmail.com>
2021-09-15 17:08:07 -0300

    Req 1, fix

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,9 +4,7 @@ def study_schedule(permanence_period, target_time):
         if not (isinstance(target_time, int) and target_time >= 0):
             return None
         for index in permanence_period:
-            start_time = index[0]
-            end_time = index[1]
-            if (start_time <= target_time <= end_time):
+            if (index[0] <= target_time <= index[1]):
                 count += 1
         return count
     except TypeError:
