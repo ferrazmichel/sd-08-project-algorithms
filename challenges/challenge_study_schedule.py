b51031bc30b7dbb2b68f45a130d032cf0fe1e639
paulo-lins <paulolins100@gmail.com>
2021-09-15 16:32:26 -0300

    Req-1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    """ Faça o código aqui.   Horário tem a maior
+    quantidade de pessoas acessando o conteúdo da
+    plataforma ao mesmo tempo"""
+    if not target_time or target_time != int:
+        return None
+    for index in range(len(permanence_period)):
+        start_time = permanence_period[index][0]
+        end_time = permanence_period[index][1]
+        if start_time <= target_time <= end_time:
+            return target_time
