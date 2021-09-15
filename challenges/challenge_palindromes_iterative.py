a6ae70710eeea83137c82c8e7997b383f5ae2ac7
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-09-15 17:36:51 -0300

    Implementa desafio palindromes_iterative.py .

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,9 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    """Faça o código aqui."""
+    if not isinstance(word, str) or len(word) == 0:
+        return False
+    reverse_word = word[::-1]
+    if reverse_word != word:
+        return False
+
+    return True
