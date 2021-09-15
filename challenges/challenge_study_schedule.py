636488f3efadd563c37211e4f0eb44925b8b223a
albertassi88 <albertassi88@gmail.com>
2021-09-15 16:31:29 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    contador = 0
+    try:
+        for time in permanence_period:
+            if target_time >= time[0] and target_time <= time[1]:
+                contador += 1
+    except TypeError:
+        return None
+    return contador
