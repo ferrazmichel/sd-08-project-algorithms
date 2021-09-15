06220f8ba7bcac97ad11d7f52bff0840418752ef
Ediberto Oliveira <edibertooliveira@aol.com>
2021-09-15 15:22:36 -0300

    feat: requirement 02

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if high_index == 1:
+        return True
+    elif (not word) or (word[low_index] != word[high_index]):
+        return False
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
