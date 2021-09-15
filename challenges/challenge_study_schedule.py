47f990ea454e31cf61da86adb3d24d89037210be
paulo-lins <paulolins100@gmail.com>
2021-09-15 16:52:11 -0300

    Req-1 Passou localmente

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,10 +2,13 @@ def study_schedule(permanence_period, target_time):
     """ Faça o código aqui.   Horário tem a maior
     quantidade de pessoas acessando o conteúdo da
     plataforma ao mesmo tempo"""
-    if not target_time or target_time != int:
+    count = 0
+    try:
+        for index in range(len(permanence_period)):
+            start_time = permanence_period[index][0]
+            end_time = permanence_period[index][1]
+            if (start_time <= target_time <= end_time):
+                count += 1
+        return count
+    except TypeError:
         return None
-    for index in range(len(permanence_period)):
-        start_time = permanence_period[index][0]
-        end_time = permanence_period[index][1]
-        if start_time <= target_time <= end_time:
-            return target_time
