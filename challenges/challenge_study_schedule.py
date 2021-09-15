4cc47bf20bbbc030889629d0e6c6a60720af9fba
jnpefo <jnpefo@gmail.com>
2021-09-15 17:34:20 -0300

    Req 1, tenta 3

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,16 +1,9 @@
 def study_schedule(permanence_period, target_time):
     try:
         count = 0
-        if not (isinstance(target_time, int)):
-            return None
-        count = teste(permanence_period,  target_time, count)
+        for index in permanence_period:
+            if (index[0] <= target_time <= index[1]):
+                count += 1
         return count
     except TypeError:
         return None
-
-
-def teste(permanence_period,  target_time, count):
-    for index in permanence_period:
-        if (index[0] <= target_time <= index[1]):
-            count += 1
-    return count
