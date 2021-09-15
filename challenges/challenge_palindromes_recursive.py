09a4aeeedc37e6e42f003d239c3ac04b793c5fb3
Leandro Lachini <leandrolachini@gmail.com>
2021-09-15 17:16:55 -0300

    task 1 and 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,20 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    lowercase_word = word.lower()
+    word_length = len(lowercase_word)
+    if word == "":
+        return False
+    if word_length < 2:
+        return True
+    elif lowercase_word[low_index] == lowercase_word[high_index]:
+        remaining_words = lowercase_word[1: word_length - 1]
+        # print(len(remaining_words) - 1)
+        return is_palindrome_recursive(
+            remaining_words, 0, len(remaining_words) - 1
+        )
+    else:
+        return False
+
+
+# word = "ANA"
+# is_palindrome_recursive(word, 0, len(word) -1)
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
-    pass
+    count = 0
+    if target_time is None:
+        # print('oi 2')
+        return None
+    for study_hour in permanence_period:
+        try:
+            is_true = study_hour[0] <= target_time <= study_hour[1]
+            # print(count)
+        except (ValueError, TypeError):
+            return None
+        count += is_true
+    return count
