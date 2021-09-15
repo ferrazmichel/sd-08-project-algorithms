f084b93e379f142e1e59ed5c3a1aa8b0b84c6747
Bruno Wesley <bruno.wesley77@gmail.com>
2021-09-15 20:33:16 -0300

    req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,13 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui.  """
+    if ((first_string == '') or (second_string == '')):
+        return False
+    if(len(first_string) != len(second_string)):
+        return False
+    for index in range(len(first_string)):
+        substring = first_string[index]
+        count_in_first = first_string.count(substring)
+        count_in_second = second_string.count(substring)
+        if(count_in_first != count_in_second):
+            return False
+    return True
