9f6a0867e3efa888cdd39f2f0e4b0667fe1438cd
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-09-15 14:59:37 -0300

    Implementa desafio study_schedule.py .

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    pass
+    if not target_time:
+        return None
+    result = 0
+    for student_in, student_out in permanence_period:
+        if not isinstance(student_in, int) or not isinstance(student_out, int):
+            return None
+        if student_in <= target_time <= student_out:
+            result += 1
+    return result
