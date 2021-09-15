dd23acadc9e475fb05a17b9fb6b670518542ccb4
jnpefo <jnpefo@gmail.com>
2021-09-15 17:10:01 -0300

    Req 1, fix

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,7 +1,7 @@
 def study_schedule(permanence_period, target_time):
     try:
         count = 0
-        if not (isinstance(target_time, int) and target_time >= 0):
+        if not (isinstance(target_time, int)):
             return None
         for index in permanence_period:
             if (index[0] <= target_time <= index[1]):
