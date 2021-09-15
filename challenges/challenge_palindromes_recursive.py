af5ced739947bcf0d1ad220ef5d85ac08fc59932
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-09-15 15:31:04 -0300

    Implementa desafio palindromes_recursive.py .

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,17 @@
+def reverse(list):
+    if len(list) < 2:
+        return list
+    else:
+        return reverse(list[1:]) + [list[0]]
+
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    """Faça o código aqui."""
+    if not isinstance(word, str) or len(word) == 0:
+        return False
+    reverse_word_list = reverse(list(word))
+    reverse_word = "".join(reverse_word_list)
+    if reverse_word != word:
+        return False
+
+    return True
