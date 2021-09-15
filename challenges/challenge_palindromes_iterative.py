5f59820fad8f50798e74444e726ee9cfa9d8d791
Ediberto Oliveira <edibertooliveira@aol.com>
2021-09-15 16:31:37 -0300

    feat: requirement 05

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,10 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    if len(word) <= 1:
+        return False
+
+    for index in range(len(word)):
+        if word[index] != word[-index - 1]:
+            return False
+
+    return True
