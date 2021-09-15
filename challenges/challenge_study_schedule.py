5c9415a2108398f04515646df6549173f99b53d2
victor_felipe <victorfelipeoliveira1@gmail.com>
2021-09-15 20:45:20 -0300

    alteracao req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,15 +1,20 @@
+def validy_period(time_period, target_time):
+    if time_period[0] == target_time or time_period[1] == target_time:
+        return 1
+    elif time_period[0] <= target_time and target_time <= time_period[1]:
+        return 1
+    return 0
+
+
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
     if type(target_time) is not int:
         return None
     contador = 0
     for time_period in permanence_period:
-        if type(time_period) is not int or type(time_period[1]) is not int:
+        if type(time_period[0]) is not int or type(time_period[1]) is not int:
             return None
-        if time_period[0] == target_time or time_period[1] == target_time:
-            contador += 1
-        elif time_period[0] <= target_time and target_time <= time_period[1]:
-            contador += 1
+        contador += validy_period(time_period, target_time)
     if contador > 0:
         return contador
     return None
