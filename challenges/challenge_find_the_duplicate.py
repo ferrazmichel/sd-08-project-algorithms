18076842fef13dea5635c40e0d650a6449d16c51
Daniel <danieldsrd@gmail.com>
2021-09-15 18:07:49 -0300

    Req 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,13 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if nums == [] or len(nums) == 1:
+        return False
+    freq_dict = {}
+    for num in nums:
+        if type(num) == str or num < 0:
+            return False
+        freq_dict[num] = freq_dict.get(num, 0) + 1
+    max_value = max(freq_dict, key=freq_dict.get)
+    min_value = min(freq_dict, key=freq_dict.get)
+    if max_value == min_value and len(freq_dict) > 1:
+        return False
+    return max_value
