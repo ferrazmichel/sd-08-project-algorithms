e3bd9a783b6eab606be3c914e4f5264ecf87b8ab
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-09-15 17:17:50 -0300

    Implementa desafio find_the_duplicate.py .

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,15 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    """Faça o código aqui."""
+    if not nums or len(nums) <= 1:
+        return False
+    frequency = {}
+    for element in nums:
+        if not isinstance(element, int) or element < 0:
+            return False
+        frequency[element] = frequency.get(element, 0) + 1
+
+    list_values = list(frequency.values())
+    list_keys = list(frequency.keys())
+    if max(list_values) == 1:
+        return False
+    return list_keys[list_values.index(max(list_values))]
