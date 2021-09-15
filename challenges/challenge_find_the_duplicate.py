a7a79c59b5282b3d11bdd13c02d68bf85363d01f
Fabio Ferreira <fabio35547@gmail.com>
2021-09-15 20:50:18 -0300

    Requisito 4 - Ajuste de complexidade

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -4,17 +4,21 @@ def check_duplicate(nums, num):
     return False
 
 
-def find_duplicate(nums):
+def get_num_duplicate(nums):
     num_duplicate = 0
-
-    if len(nums) <= 1 or any(isinstance(num, str) or num < 0 for num in nums):
-        return False
-
     for num in nums:
         if (check_duplicate(nums, num)):
             if (num_duplicate > 0 and num_duplicate != num):
-                return False
+                return 0
             num_duplicate = num
+    return num_duplicate
+
+
+def find_duplicate(nums):
+    if len(nums) <= 1 or any(isinstance(num, str) or num < 0 for num in nums):
+        return False
+
+    num_duplicate = get_num_duplicate(nums)
 
     if (num_duplicate > 0):
         return num_duplicate
