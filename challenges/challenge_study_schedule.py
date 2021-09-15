28dc2460083a179f5cb4b5c9e1ed4b48f36878b3
Daniel <danieldsrd@gmail.com>
2021-09-15 17:05:58 -0300

    Req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count = 0
+    try:
+        for period in permanence_period:
+            if target_time >= period[0] and target_time <= period[1]:
+                count += 1
+    except TypeError:
+        return None
+    return count
