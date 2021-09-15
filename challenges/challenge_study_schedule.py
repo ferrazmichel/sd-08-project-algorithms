55ec84ec59594df775704a046606c98fe3d6701e
Bruno Wesley <bruno.wesley77@gmail.com>
2021-09-15 20:01:32 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    times = 0
+    if target_time is None:
+        return None
+    for student in permanence_period:
+        if type(student[0]) is not int or type(student[1]) is not int:
+            return None
+        if student[0] <= target_time <= student[1]:
+            times += 1
+
+    return times
