87687603d7e4ae0e7a2f9a151d4902f354cfe4f6
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2021-09-15 15:41:30 -0300

    commit inicial - requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,16 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+
+    count = 0
+
+    for student in permanence_period:
+        if (
+            student[0] is None
+            or student[1] is None
+            or target_time is None
+        ):
+            return None
+        elif student[0] <= target_time <= student[1]:
+            count += 1
+
+    return count
