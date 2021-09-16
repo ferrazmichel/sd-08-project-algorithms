5ab6ebe5b9f4572c047879dc7095ac2ac66e375b
victor_felipe <victorfelipeoliveira1@gmail.com>
2021-09-16 11:35:15 -0300

    req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,23 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if type(word) != list:
+        if type(word) is not str or len(word) == 0:
+            return False
+        word = [word, word]
+
+    meio = len(word[0]) // 2
+    if low_index < meio and high_index >= meio:
+        word_1 = list(word[1])
+
+        temp = word_1[low_index]
+        word_1[low_index] = word_1[high_index]
+        word_1[high_index] = temp
+        low_index += 1
+        high_index -= 1
+        word_1 = str("".join(word_1))
+        word[1] = word_1
+        if low_index < meio and high_index >= meio:
+            is_palindrome_recursive(word, low_index, high_index)
+
+    if word[0] == word[1]:
+        return True
+    return False
