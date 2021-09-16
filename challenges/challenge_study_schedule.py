411b78697c2e7853ced7060bfeb526882915ce9f
Giovanna Betti <giovannabetti@gmail.com>
2021-09-16 15:04:48 -0300

    req 01. algoritmo de busca usando forca bruta 0(n)

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    count = 0
+    try:
+        for time in permanence_period:
+            if time[0] <= target_time <= time[1]:
+                count += 1
+    except TypeError:
+        return None
+    return count
