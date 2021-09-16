6cbeb5eec0d5acf830c879c63dc0dd51f295c8d1
caiomzrocha <caiomzrocha@gmail.com>
2021-09-15 23:06:42 -0300

    [REQ-04] Implementa a funçao find_duplicate.

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,12 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    set_of_numbers = set({})
+    for num in nums:
+        if type(num) != int or num < 1:
+            return False
+        if num in set_of_numbers:
+            return num
+        else:
+            set_of_numbers.add(num)
+
+    return False
