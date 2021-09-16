40bfd7619c78faf400be97414fc510a31cb3b0e6
Giovanna Betti <giovannabetti@gmail.com>
2021-09-16 15:40:17 -0300

    req 02 - recursividade

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,16 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    try:
+        if word == "" or word[low_index] != word[high_index]:
+            return False
+        elif low_index == high_index:
+            return True
+        else:
+            return (
+                is_palindrome_recursive(
+                    word,
+                    low_index + 1,
+                    high_index - 1)
+            )
+    except IndexError:
+        return False
