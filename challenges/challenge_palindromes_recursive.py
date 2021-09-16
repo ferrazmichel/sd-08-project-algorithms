4be0e40e238c7740fbcc7fc53ce96594a28f8863
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-09-15 22:00:55 -0300

    adicionando requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,16 +1,26 @@
-def recursive_palindrome(word):
+# def recursive_palindrome(word):
+#     if len(word) == 0:
+#         return False
+#     if len(word) < 2:
+#         return word
+#     else:
+#         return recursive_palindrome(word[1:]) + word[0]
+
+
+def is_palindrome_recursive(word, low_index, high_index):
     if len(word) == 0:
         return False
-    if len(word) < 2:
-        return word
-    else:
-        return recursive_palindrome(word[1:]) + word[0]
 
+    # caso a palavra na mesma posição oposta for diferente retorna False
+    if word[high_index] != word[low_index]:
+        return False
 
-def is_palindrome_recursive(word):
-    reversed_word = recursive_palindrome(word)
-    if(reversed_word == word):
+    """quando os index tanto o low quanto o high chegarem ao meio
+    (quando for ímpar) ou o low for maior que o high (quando for par)
+    significa que é palíndromo pois percorreram a primeira e a segunda
+    metade comparando os caracteres opostos"""
+    if high_index == low_index or low_index > high_index:
         return True
-    return False
 
-
+    # vai chamando a função e aumentando low e diminuindo o high
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
