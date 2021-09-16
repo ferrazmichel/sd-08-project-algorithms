5dae42e8bdbf48c6e1be7dc33582ec8f7c7a578a
caiomzrocha <caiomzrocha@gmail.com>
2021-09-15 22:42:17 -0300

    [REQ-03] Implementa a funçao is_anagram.

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,4 +1,14 @@
+def count_letters(text):
+    number_of_each_letter = {}
+    for letter in text:
+        if letter in number_of_each_letter:
+            number_of_each_letter[letter] += 1
+        else:
+            number_of_each_letter[letter] = 1
+
+    return number_of_each_letter
+
+
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
-
-# change
+    return count_letters(first_string) == count_letters(second_string)
