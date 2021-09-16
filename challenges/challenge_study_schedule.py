b197c8fc0547975bcb35fb2ffaf4e8386fe6cf0c
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-09-16 15:18:25 -0300

    implementa study_schedule - desafio 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
-    pass
+    students_on_target_time = 0
+    if target_time is None:
+        return None
+    for interval in permanence_period:
+        if not isinstance(interval[0], int) or not isinstance(
+            interval[1], int
+        ):
+            return None
+        if interval[0] <= target_time <= interval[1]:
+            students_on_target_time += 1
+    return students_on_target_time
