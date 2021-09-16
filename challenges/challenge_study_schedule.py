52262c9e4b7e98d30b7251ebaa40f1b24f0461bd
VitorLimaRios <vitorlimarios999@gmail.com>
2021-09-16 19:52:53 -0300

    add req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    try:
+        times_shown = 0
+        for i in permanence_period:
+            if i[0] <= target_time <= i[1]: times_shown += 1
+        return times_shown
+    except:
+        return None
+    
