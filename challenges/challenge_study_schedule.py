38e2927fb36ebb02e83e81a1e8f14649f17c0a28
victor Farias <victorfarias.tv@gmail.com>
2021-09-16 15:53:47 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ FIRST COMMIT """
-
+    result = 0
+    if target_time is None:
+        return None
+    for i in permanence_period:
+        if not isinstance(i[0], int) or not isinstance(i[1], int):
+            return None
+        elif target_time >= i[0] and target_time <= i[1]:
+            result += 1
+    return result
