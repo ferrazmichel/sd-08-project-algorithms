0b81e93ae5658b3cd99faaa2d98c4f45cd7941bd
hugobrr <hugobrr@gmail.com>
2021-09-16 05:11:09 -0300

    Teste 1 refatorado passando tudo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,18 +1,19 @@
 def study_schedule(permanence_period, target_time):
     counter = 0
-    if target_time is not None:
-        for moment in permanence_period:
-            if (
-                moment[0] is None
-                or moment[1] is None or
-                not isinstance(moment[0], int) or
-                not isinstance(moment[1], int)
-            ):
-                return None
-            if (
-                target_time >= moment[0]
-                and target_time <= moment[1]
-            ):
+    for moment in permanence_period:
+        # if (
+        #     moment[0] is None
+        #     or moment[1] is None
+        #     or not isinstance(moment[0], int)
+        #     or not isinstance(moment[1], int)
+        # ):
+        #     return None
+        # if target_time >= moment[0] and target_time <= moment[1]:
+        try:
+            result = target_time >= moment[0] and target_time <= moment[1]
+        except TypeError:
+            return None
+        else:
+            if result:
                 counter += 1
-        return counter
-    return None
+    return counter
