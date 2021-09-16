9ce1e9d0d54bccb632380c7537e911990ee00e6c
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-09-16 16:43:07 -0300

    implementa parcialmente is_palindrome_iteractive

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,10 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    if not word:
+        return False
+    if len(word) == 1:
+        return True
+    reversed_word = ""
+    for i in range(len(word)-1, -1, -1):
+        reversed_word += word[i]
+    return word == reversed_word
