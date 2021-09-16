7c288e720e2d3d404222d088cfbedc7bc9e3272b
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-09-16 17:06:45 -0300

    feat: complete requirement 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,25 @@
+# função construída conforme conteúdo do course
+def bubble_sort(string):
+    has_swapped = True
+    num_of_iterations = 0
+
+    while has_swapped:
+        has_swapped = False
+
+        for i in range(len(string) - num_of_iterations - 1):
+            if string[i] > string[i + 1]:
+                string[i], string[i + 1] = string[i + 1], string[i]
+                has_swapped = True
+        num_of_iterations += 1
+
+    return string
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if not (first_string and second_string):
+        return False
+
+    if bubble_sort(list(first_string)) == bubble_sort(list(second_string)):
+        return True
+
+    return False
