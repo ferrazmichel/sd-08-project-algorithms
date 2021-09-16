634dceb94349dc16883394b7d72a31dedc50c670
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-09-16 18:45:29 -0300

    feat: complete requirement 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,6 @@
+# https://www.geeksforgeeks.org/python-program-check-string-palindrome-not/
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+
+    return word == word[::-1]
