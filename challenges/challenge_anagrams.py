5f078c09a230df45a91ecb007d408bb62461274c
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-09-16 17:17:36 -0300

    fix: requirement 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,25 +1,37 @@
 # função construída conforme conteúdo do course
-def bubble_sort(string):
-    has_swapped = True
-    num_of_iterations = 0
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+    mid = len(array) // 2
+    left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
+    return merge(left, right, array.copy())
 
-    while has_swapped:
-        has_swapped = False
 
-        for i in range(len(string) - num_of_iterations - 1):
-            if string[i] > string[i + 1]:
-                string[i], string[i + 1] = string[i + 1], string[i]
-                has_swapped = True
-        num_of_iterations += 1
+def merge(left, right, merged):
+    left_cursor, right_cursor = 0, 0
 
-    return string
+    while left_cursor < len(left) and right_cursor < len(right):
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
 
 
 def is_anagram(first_string, second_string):
     if not (first_string and second_string):
         return False
 
-    if bubble_sort(list(first_string)) == bubble_sort(list(second_string)):
+    if merge_sort(list(first_string)) == merge_sort(list(second_string)):
         return True
 
     return False
