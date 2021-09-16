a2018a00a9537f2940977bed6668bf599c63b77b
Rita Jeveaux <ritajeveaux@gmail.com>
2021-09-16 15:00:47 -0300

    Req2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word or word[high_index] != word[low_index]:
+        return False
+    if low_index == high_index:
+        return True
+    low_index += 1
+    high_index -= 1
+    return is_palindrome_recursive(word, low_index, high_index)
