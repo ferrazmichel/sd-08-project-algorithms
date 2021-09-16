4f6772eca23357cfd4f934b977b8c54b00cf712b
Giovanna Betti <giovannabetti@gmail.com>
2021-09-16 20:07:00 -0300

    adicionando leituras e videos de referencias usadas para construcao de codigo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -14,3 +14,18 @@ def is_palindrome_recursive(word, low_index, high_index):
             )
     except IndexError:
         return False
+
+# References
+# https://www.geeksforgeeks.org/recursive-function-check-string-palindrome/
+# https://stackoverflow.com/questions/952110/recursive-function-palindrome-in-python
+# From a general algorithm perspective, the recursive function has 3 cases:
+# 1) 0 items left. Item is a palindrome, by identity.
+# 2) 1 item left. Item is a palindrome, by identity.
+# 3) 2 or more items. Remove first and last
+#  item. Compare. If they are the same,
+# call function on what's left of string. If first and last are not the same,
+# item is not a palindrome.
+# The implementation of the function itself
+# is left as an exercise to the reader :)
+# PYTHON Recursion - Palindrome recursive function:
+# https://www.youtube.com/watch?v=92UyahoAne4
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -8,3 +8,9 @@ def study_schedule(permanence_period, target_time):
     except TypeError:
         return None
     return count
+
+# References
+# https://panda.ime.usp.br/algoritmos/static/algoritmos/06-busca.html
+# https://algoritmosempython.com.br/cursos/algoritmos-python/pesquisa-ordenacao/pesquisa-linear/
+# https://pt.stackoverflow.com/questions/388284/algor%C3%ADtmo-de-busca-bin%C3%A1ria-em-python
+# https://pt.khanacademy.org/computing/computer-science/algorithms/binary-search/a/implementing-binary-search-of-an-array
