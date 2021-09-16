271b534d0031f9058c51dc51f6c5f26d7301c325
Rita Jeveaux <ritajeveaux@gmail.com>
2021-09-16 16:28:57 -0300

    Req5

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,4 @@
 def is_anagram(first_string, second_string):
-    pass
+    if not first_string or not second_string:
+        return False
+    return sorted(first_string.lower()) == sorted(second_string.lower())
