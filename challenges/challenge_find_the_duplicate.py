317a5e2dd8bc8cf8deb158b78006e366afd47a7f
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-09-16 17:30:20 -0300

    implementa parcialmente find_duplicate - desafio 04

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,8 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    if isinstance(nums, int) or isinstance(nums, str) or len(nums) > 2:
+        return False
+    for i in range(len(nums) - 2):
+        if nums[i] in nums[i + 1:]:
+            return nums[i]
+    return False
