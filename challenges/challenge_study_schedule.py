df3f6174e05cb8dd986669f521581ab10a7919a9
VitorLimaRios <vitorlimarios999@gmail.com>
2021-09-16 19:55:36 -0300

    fix black

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,8 +2,8 @@ def study_schedule(permanence_period, target_time):
     try:
         times_shown = 0
         for i in permanence_period:
-            if i[0] <= target_time <= i[1]: times_shown += 1
+            if i[0] <= target_time <= i[1]:
+                times_shown += 1
         return times_shown
     except:
         return None
-    
