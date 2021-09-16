efb6223021f2cf97ba2dd661c8319672f6ea5256
paulo-lins <paulolins100@gmail.com>
2021-09-16 15:28:55 -0300

    req_3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -6,31 +6,35 @@ def is_anagram(first_string, second_string):
     lista1 = list(first_string)
     if len(lista) != len(lista1):
         return False
-    # loop mais externo começando de (len_lista - 1) até 1 com decremento -1
+
+    order_list(lista)
+    order_list(lista1)
     # o "if" testa se a string acessada pelo índice (j + 1)
     # precede a string acessada pelo índice "j"
-    j = 0
-    if(lista[j] > lista[j + 1]):
-        # faz a troca dos elementos
-        aux = lista[j]
-        lista[j] = lista[j + 1]
-        lista[j + 1] = aux
-        j += 1
-
-        # loop mais externo começando de (len_lista1 - 1) até 1 com decremento -1
-        # o "if" testa se a string acessada pelo índice (j + 1)
-        # precede a string acessada pelo índice "j"
-    k = 0
-    if(lista1[k] > lista1[k + 1]):
-        # faz a troca dos elementos
-        aux1 = lista1[k]
-        lista1[k] = lista1[k + 1]
-        lista1[k + 1] = aux1
-        k += 1
+    # k = 0
+    # if lista1[k] > lista1[k + 1]:
+    #     # faz a troca dos elementos
+    #     aux1 = lista1[k]
+    #     lista1[k] = lista1[k + 1]
+    #     lista1[k + 1] = aux1
+    #     k += 1
 
     if lista == lista1:
         return True
 
-    if lista != lista1:
-        return False
+    # if lista != lista1:
+    #     return False
     return is_anagram(first_string, second_string)
+
+
+def order_list(word_list):
+    j = 0
+    if word_list[j] > word_list[j + 1]:
+        # o "if" testa se a string acessada pelo índice (j + 1)
+        # precede a string acessada pelo índice "j"
+        # faz a troca dos elementos
+        aux = word_list[j]
+        word_list[j] = word_list[j + 1]
+        word_list[j + 1] = aux
+        j += 1
+    return word_list
