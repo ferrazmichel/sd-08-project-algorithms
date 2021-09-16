040efcb9144de6ad8fc7208606ba662353778407
victor_felipe <victorfelipeoliveira1@gmail.com>
2021-09-16 12:21:13 -0300

    fix req 2

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,21 @@
-def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+# def is_anagram(first_string, second_string):
+#     """ Faça o código aqui. """
+#     f_string = list(first_string)
+#     s_string = list(second_string)
+
+#     if len(f_string) != len(s_string):
+#         return False
+
+#     for letter in f_string:
+#         for indice in range(len(s_string)):
+#             if s_string[indice] == letter:
+#                 s_string.pop(indice)
+#                 break
+#     if len(s_string) == 0:
+#         return True
+#     return False
+
+
+# # # s_string = dict(enumerate(list("CORAGEM")))
+# # # print(s_string)
+# # print(is_anagram("AMO", "RMA"))
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,15 @@
+def change_letter(low_index, high_index, word):
+    word_1 = list(word[1])
+    temp = word_1[low_index]
+    word_1[low_index] = word_1[high_index]
+    word_1[high_index] = temp
+    low_index += 1
+    high_index -= 1
+    word_1 = str("".join(word_1))
+    word[1] = word_1
+    return low_index, high_index, word
+
+
 def is_palindrome_recursive(word, low_index, high_index):
     if type(word) != list:
         if type(word) is not str or len(word) == 0:
@@ -6,17 +18,11 @@ def is_palindrome_recursive(word, low_index, high_index):
 
     meio = len(word[0]) // 2
     if low_index < meio and high_index >= meio:
-        word_1 = list(word[1])
+        low_index, high_index, word = change_letter(
+            low_index, high_index, word
+        )
 
-        temp = word_1[low_index]
-        word_1[low_index] = word_1[high_index]
-        word_1[high_index] = temp
-        low_index += 1
-        high_index -= 1
-        word_1 = str("".join(word_1))
-        word[1] = word_1
-        if low_index < meio and high_index >= meio:
-            is_palindrome_recursive(word, low_index, high_index)
+        is_palindrome_recursive(word, low_index, high_index)
 
     if word[0] == word[1]:
         return True
