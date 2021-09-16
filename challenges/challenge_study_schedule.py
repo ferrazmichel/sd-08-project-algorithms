419e0cd2c3672ffa901138eaeb09bc7e5a9ca345
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-09-16 00:20:48 -0300

    fix: requirement 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,10 +4,10 @@ def study_schedule(permanence_period, target_time):
 
     target_time_occurrence = 0
 
-    for time_range in permanence_period:
-        if not all(isinstance(n, int) for n in time_range):
+    for login_time, logoff_time in permanence_period:
+        if not (isinstance(login_time, int) and isinstance(logoff_time, int)):
             return None
-        if time_range[0] <= target_time <= time_range[1]:
+        if login_time <= target_time <= logoff_time:
             target_time_occurrence += 1
 
     return target_time_occurrence
