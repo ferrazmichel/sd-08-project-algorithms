492043967907c8ab1fbbb8db59f5254773d539f3
caiomzrocha <caiomzrocha@gmail.com>
2021-09-15 23:26:36 -0300

    [REQ-05] Implementa a funçao is_palindrome_iterative.

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,13 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+
+    if len(word) == 1:
+        return True
+
+    for index in range(len(word) // 2):
+        if word[index] != word[-(index + 1)]:
+            return False
+
+    return True
