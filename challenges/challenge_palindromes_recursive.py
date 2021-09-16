b8e1891d9f2eb7a5e101adf6e0bd7f129253abad
Bruno Wesley <bruno.wesley77@gmail.com>
2021-09-15 21:07:36 -0300

    req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if not word:
+        return False
+
+    if (low_index < high_index):
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:
+        return True
