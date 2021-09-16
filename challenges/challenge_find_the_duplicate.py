bf9d932c22b5f1a5cb2d3f524b66eb7af3ba6f21
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-09-16 18:21:04 -0300

    feat: complete requirement 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,14 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if not (
+        all(isinstance(num, int) for num in nums) and
+        all(num > 0 for num in nums)
+    ):
+        return False
+
+    sorted_nums = sorted(nums)
+
+    for index in range(len(sorted_nums) - 1):
+        if sorted_nums[index] == sorted_nums[index + 1]:
+            return sorted_nums[index]
+
+    return False
