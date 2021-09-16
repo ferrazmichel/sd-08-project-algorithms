da4b29c4a49d246d5d97d6a4e77bcb6838810206
Rita Jeveaux <ritajeveaux@gmail.com>
2021-09-16 15:21:16 -0300

    Req5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,11 @@
 def is_palindrome_iterative(word):
-    pass
+    high_index = len(word) - 1
+    low_index = 0
+    if not word:
+        return False
+    for i in range(len(word)//2):
+        if word[high_index] != word[low_index]:
+            return False
+        low_index += 1
+        high_index -= 1
+    return True
