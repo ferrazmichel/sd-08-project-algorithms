d2cc6cb4c767580115346a2ef143f3acfd4dc851
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-09-16 15:30:56 -0300

    implementa parcialmente is_anagram - desafio 02

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,20 @@
+def sort_string(string, criteria=None):
+    """recebe uma string e ordena as letras
+    em ordem alfabética"""
+    string_list = list(string)
+    for index in range(len(string_list) - 1):
+        for index_2 in range(len(string_list) - 1):
+            if string_list[index_2] > string_list[index_2 + 1]:
+                string_list[index_2], string_list[index_2 + 1] = (
+                    string_list[index_2 + 1],
+                    string_list[index_2],
+                )
+    return "".join(string_list)
+
+
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    return sort_string(first_string) == sort_string(second_string)
+
+
+print(sort_string("caramelo"))
