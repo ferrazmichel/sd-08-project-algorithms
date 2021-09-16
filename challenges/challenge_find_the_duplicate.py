8136e37449524af734adbcb511568cec01288a43
hugobrr <hugobrr@gmail.com>
2021-09-16 15:55:31 -0300

    com ajuda do Bux refatorando o req 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,22 +1,36 @@
 def find_duplicate(nums):
     size = len(nums)
-    if size >= 2 and nums[0] > 0:
-        recursive(nums)
-    return False
+    if size == 0:
+        return False
+    tested_number = size // 2
+    try:
+        result = nums[tested_number] < 1
+    except TypeError:
+        return False
+    else:
+        if result:
+            return False
+    bigger = [i for i in nums if i > tested_number]
+    smaller = [i for i in nums if i <= tested_number]
+    result_1 = recursive(bigger)
+    result_2 = recursive(smaller)
+    return result_1 or result_2
 
 
 def recursive(nums):
     size = len(nums)
+    bigger = []
+    smaller = []
     if size >= 2:
-        bigger = []
-        smaller = []
         tested_number = nums[0]
         for number in nums[1:]:
-            if number > tested_number:
-                bigger.append(number)
+            if number == tested_number:
+                return number
             elif number < tested_number:
                 smaller.append(number)
             else:
-                return tested_number
-        recursive(smaller)
-        recursive(bigger)
+                bigger.append(number)
+        result_1 = recursive(bigger)
+        result_2 = recursive(smaller)
+        return result_1 or result_2
+    return False
