22333f2bf4f0bb6529d96e604639a3ddc4573c01
paulo-lins <paulolins100@gmail.com>
2021-09-15 21:20:55 -0300

    req - 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,8 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    if not word:
+        return False
+    if word[::-1] == word:
+        return True
+    if word[::-1] != word:
+        return False
