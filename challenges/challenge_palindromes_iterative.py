efa6f25a3fbdea8a001aca2e086c205d64a7c643
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2021-09-16 03:01:40 -0300

    requisito 2 E 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,6 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+
+    # https://stackoverflow.com/questions/9027862/what-does-listxy-do
+
+    return word != "" and word == word[::-1]
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
+def reverse_word(word, low_index, high_index):
+    length_word = len(word[low_index:high_index])
+
+    if length_word < 2:
+        return word
+    else:
+        return reverse_word(word[1:], low_index, high_index) + word[0]
+
+
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+
+    return reverse_word(word, low_index, high_index) == word
