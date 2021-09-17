bed5b8582a7559490650ba6200d7c93718cb91e2
Seu nome <luiza.dilly@gmail.com>
2021-09-17 17:38:31 -0300

    req 2 in progress

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+# referencia https://www.delftstack.com/pt/howto/python/python-palindrome/
+    if word == '' and word[low_index] != word[high_index]:
+        return False
+    if word == "".join(reversed(word)):
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return True
