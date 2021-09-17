f8d02db38546369758ec59584b9287c12dc538f5
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-09-17 15:34:18 -0300

    palindromos recursivos req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
+def reversed(word, low_index, high_index):
+    if len(word) < 2:
+        return word
+    else:
+        return word[high_index] + reversed(word[low_index:high_index], low_index, high_index -1)
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    return word == reversed(word, low_index, high_index)
+
+# print(is_palindrome_recursive('arara', 0,4))
+# print(is_palindrome_recursive('mamao', 0,4))
