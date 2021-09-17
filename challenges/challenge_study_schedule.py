18d3418ed497e5b67aa4d6783113c750c6636048
liisaambrosen <liisa.ambrosen@gmail.com>
2021-09-17 18:40:31 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time:
+        return None
+
+    count = 0
+
+    for tup in permanence_period:
+        if not tup[0] or not tup[1]:
+            return None
+
+        if target_time >= tup[0] and target_time <= tup[1]:
+            count += 1
+
+    return count
