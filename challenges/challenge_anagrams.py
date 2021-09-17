33ad3c0d549a2c0677f4c13cb57c1f1c47c9ea47
VitorLimaRios <vitorlimarios999@gmail.com>
2021-09-17 16:56:20 -0300

    add req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,13 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if not first_string or not second_string:
+        return False
+
+    while first_string:
+        character = first_string[0]
+        first_arr = first_string.split(character)
+        second_arr = second_string.split(character)
+        if len(first_arr) != len(second_arr):
+            return False
+        first_string = "".join(first_arr)
+        second_string = "".join(second_arr)
+    return True
