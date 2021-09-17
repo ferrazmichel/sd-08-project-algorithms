9c16becd19e916c77ae58694086846856a19a26b
jnpefo <jnpefo@gmail.com>
2021-09-16 23:41:21 -0300

    Req 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,10 @@
+import collections
+
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if len(nums) == 0 or isinstance(nums[0], str) or nums[0] == -1:
+        return False
+    try:
+        return ([item for item, count in collections.Counter(nums).items() if count > 1][0])
+    except IndexError:
+        return False
