53768d0a9212014bd454fe3624ee82833c2e2667
jnpefo <jnpefo@gmail.com>
2021-09-17 19:09:09 -0300

    Req 5, fix

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -5,7 +5,7 @@ def find_duplicate(nums):
     if len(nums) == 0 or isinstance(nums[0], str) or nums[0] == -1:
         return False
     try:
-        return ([item for item, count in 
+        return ([item for item, count in
         collections.Counter(nums).items() if count > 1][0])
     except IndexError:
         return False
