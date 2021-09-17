990838c7dae20af67d51a887ac87906a18adb151
liisaambrosen <liisa.ambrosen@gmail.com>
2021-09-17 19:05:41 -0300

    Requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if len(word) == 0 or high_index < low_index:
+        return False
+    if high_index == low_index:
+        return True
+    if word[high_index] != word[low_index]:
+        return False
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
