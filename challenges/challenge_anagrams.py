07c1b8ac59e6efc3f0053837362ce25cb2467874
Seu nome <luiza.dilly@gmail.com>
2021-09-17 19:06:48 -0300

    req 3 in progress

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,7 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if first_string == '' or second_string == '':
+        return False
+    if first_string != second_string:
+        return False
+    if first_string == second_string:
+        return True
