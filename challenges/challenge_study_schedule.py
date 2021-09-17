e22584d9020de91505a21a1eb3e0b717e4c4a860
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-09-17 02:16:38 -0300

    Exercise 1 - done

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    try:
+        occurrences = 0
+        for schedule_range in permanence_period:
+            if (
+                schedule_range[0] <= target_time
+                and schedule_range[1] >= target_time
+            ):
+                occurrences += 1
+        return occurrences
+    except TypeError:
+        return None
