86ecd9c5f263fe9e49ba75ff8e7e6dd694135a65
Seu nome <luiza.dilly@gmail.com>
2021-09-17 18:39:23 -0300

    req 2 pronto, valeu buxx <3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,7 +1,20 @@
 def is_palindrome_recursive(word, low_index, high_index):
 # referencia https://www.delftstack.com/pt/howto/python/python-palindrome/
-    if word == '' and word[low_index] != word[high_index]:
+    if word == "":
         return False
-    if word == "".join(reversed(word)):
+    if (word[low_index] != word[high_index]):
+        return False
+    if low_index == high_index:
+        return True
+    if low_index > high_index:
+        return False
+    if (word[low_index] == word[high_index]):
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
-    return True
+
+
+# ABCBA
+# is_palindrome_recursive("ABCCBA", 0, 5)
+# A B C  C B A, 0, 5
+# A B C  C B A, 1, 4
+# A B C  C B A, 2, 3
+
