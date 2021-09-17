d77e992ad511256252c56e6397035804341962e1
Bruno Pedrosa <bgpedrosas@gmail.com>
2021-09-17 16:41:10 -0300

    all mandatory reqs
    Signed-off-by: Bruno Pedrosa <bgpedrosas@gmail.com>

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,8 @@
+from collections import Counter
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if not bool(first_string) or not bool(
+      second_string) or len(first_string) != len(second_string):
+        return False
+    return Counter(first_string) == Counter(second_string)
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count = 0
+    for (start, end) in permanence_period:
+        if not isinstance(
+            target_time, int
+        ) or not isinstance(start, int) or not isinstance(end, int):
+            return None
+
+        if start <= target_time <= end:
+            count += 1
+
+    return count
