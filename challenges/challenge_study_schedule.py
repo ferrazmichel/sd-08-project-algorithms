5bdb33a4782475fc81b2d911c8c88675b213db3f
emassaki <massakihirayama@gmail.com>
2021-09-18 15:38:20 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    """Faça o código aqui."""
+    if type(target_time) != int:
+        return None
+
+    number_of_students = 0
+    for permanence in permanence_period:
+        checkin, checkout = permanence
+        if type(checkin) != int or type(checkout) != int:
+            return None
+        if checkin <= target_time <= checkout:
+            number_of_students += 1
+    return number_of_students
