9f8908da671545f489774a95bb465d10a2835e38
Tiago <tiagaoalb@gmail.com>
2021-09-18 11:21:33 -0300

    fix issue

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -5,12 +5,15 @@ def quick_sort(list):
         return []
     else:
         pivot = list[0]
-        less = [x for x in list     if x <  pivot]
+        less = [x for x in list if x < pivot]
         more = [x for x in list[1:] if x >= pivot]
         return quick_sort(less) + [pivot] + quick_sort(more)
 
+
 def is_anagram(first_string, second_string):
-    first_string, second_string = quick_sort(list(first_string)), quick_sort(list=(second_string))
+    first_string, second_string = quick_sort(list(first_string)), quick_sort(
+        list=(second_string)
+    )
     if first_string == second_string:
         return True
     else:
