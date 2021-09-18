59655a7753e630f23671285a479b4ced5fe26e33
Tiago <tiagaoalb@gmail.com>
2021-09-18 09:51:19 -0300

    add 1st requirement

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui . """
+    student_count = 0
+    try:
+        for row in permanence_period:
+            if row[0] <= target_time <= row[1]:
+                student_count += 1
+    except TypeError:
+        return None
+    return student_count
