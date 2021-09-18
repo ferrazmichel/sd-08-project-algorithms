015bdeeb18183fb75c126d11ca9b1585289bbaba
Digo-Castro <digo_castro@outlook.com>
2021-09-18 15:44:48 -0300

    [Algorithm]

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,10 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first_string_array = list(first_string)
+    second_string_array = list(second_string)
+
+    for char in first_string_array:
+        if char in second_string_array:
+            second_string_array.remove(char)
+        else:
+            return False
+    return len(second_string_array) == 0
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    time_count = 0
+    for (start, end) in permanence_period:
+        try:
+            if start <= target_time <= end:
+                time_count += 1
+        except TypeError:
+            return None
+    return time_count
