938a2d46d75ae7b0b9f371031a379cb08fc6110c
Rita Jeveaux <ritajeveaux@gmail.com>
2021-09-18 18:26:22 -0300

    Req3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -14,7 +14,7 @@ def merge(left, right, merged):
     return merged
 
 
-def mergesort(array):
+def mergesort(array):   # complexidade de O(n log n)
     if len(array) <= 1:
         return array
     mid = len(array) // 2
