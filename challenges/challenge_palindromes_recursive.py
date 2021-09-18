38225551b61ec7991e17a3c242373d39b2071e11
Mateus Coury <mateuscoury@gmail.com>
2021-09-18 11:05:37 -0300

    requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -3,13 +3,16 @@ def is_palindrome_recursive(word, low_index, high_index):
     if word == "":
         return False
 
-    if(low_index == high_index):
+    if low_index == high_index:
         return True
 
-    if word[low_index] == word[high_index]:
-        is_palindrome_recursive(word, low_index+1, high_index-1)
-    else:
+    if word[low_index] != word[high_index]:
         return False
 
+    return is_palindrome_recursive(word, low_index+1, high_index-1)
 
-is_palindrome_recursive('ANA', 0, len('ANA') - 1)
+
+#       "S  O  C  O  S"
+#        0           4  TRUE
+#           1     3     TRUE
+#              2        TRUE PAROU
