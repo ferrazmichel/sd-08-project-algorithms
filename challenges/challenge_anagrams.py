b23f2e57448a518447b7cc06fbd85641c237644e
Mateus Coury <mateuscoury@gmail.com>
2021-09-18 12:47:18 -0300

    refatorando o ultimo

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,14 @@
+def quantity_of_letters(word):
+    letter_quantity = {}
+
+    for letter in word:
+        if letter in letter_quantity:
+            letter_quantity[letter] = letter_quantity[letter]+1
+        else:
+            letter_quantity[letter] = 1
+    return letter_quantity
+
+
 def is_anagram(first_string, second_string):
 
     if first_string == "" or second_string == "":
@@ -5,13 +16,7 @@ def is_anagram(first_string, second_string):
     if len(first_string) != len(second_string):
         return False
 
-    letter_quantity = {}
-
-    for letter in first_string:
-        if letter in letter_quantity:
-            letter_quantity[letter] = letter_quantity[letter]+1
-        else:
-            letter_quantity[letter] = 1
+    letter_quantity = quantity_of_letters(first_string)
 
     for item in second_string:
         if item not in letter_quantity or letter_quantity[item] == 0:
