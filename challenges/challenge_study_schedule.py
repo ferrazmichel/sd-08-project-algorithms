04f043d2bc0dea37843918d7cc38d725061e7ce0
Rita Jeveaux <ritajeveaux@gmail.com>
2021-09-18 20:43:34 -0300

    Req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,9 +1,8 @@
 def study_schedule(permanence_period, target_time):
-    try:
-        counter = 0
-        for student in permanence_period:
-            if student[0] <= target_time and student[1] >= target_time:
-                counter += 1
-        return counter
-    except TypeError:
+    counter = 0
+    if not target_time or not permanence_period:
         return None
+    for student in permanence_period:
+        if student[0] <= target_time and student[1] >= target_time:
+            counter += 1
+    return counter
