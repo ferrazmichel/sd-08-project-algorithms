ec9ef5b7490cfc79b02f71f3488d84042aeef9eb
Mateus Coury <mateuscoury@gmail.com>
2021-09-18 11:37:28 -0300

    parte 2

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,23 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if word == '':
+        return False
+
+    reverseWord = word[::-1]
+
+    if reverseWord == word:
+        return True
+    else:
+        return False
+
+    """ if word == '':
+        return False
+    last_index = len(word)-1
+    start_index = 0
+    while start_index != last_index:
+        if start_index == last_index:
+            break
+        if word[start_index] == word[last_index]:
+            last_index = last_index - 1
+            start_index = start_index + 1
+        else:
+            return False """
---
+++ challenges/tempCodeRunnerFile.py
@@ -0,0 +1,18 @@
+def is_palindrome_iterative(word):
+    """ Faça o código aqui. """
+
+    reverseWord = word[::-1]
+    print(reverseWord)
+
+
+"""  if word == '':
+        return False
+    
+
+    last_index = len(word)-1
+    newWord = ''
+    for letter in word:
+        letter == word[last_index]
+        newWord = newWord+letter
+"""
+is_palindrome_iterative("mano")
