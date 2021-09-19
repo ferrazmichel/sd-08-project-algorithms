8265ddd5662d307db8e2343444d39c897cfb7490
tandy <tandyfw@gmail.com>
2021-09-19 08:18:38 -0300

    Requisito 1 concluido

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
+import numbers
+
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not isinstance(target_time, numbers.Number):
+        return None
+    result = 0
+    try:
+        for period in permanence_period:
+            if period[1] >= target_time >= period[0]:
+                result += 1
+        return result
+    except TypeError:
+        return None
