9870c8f92dd5ccf8a43906c4f1ec9bff6480b6d8
tandy <tandyfw@gmail.com>
2021-09-19 10:12:09 -0300

    Requisito 2 concluido

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word == '':
+        return False
+    if len(word) < 2:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    return is_palindrome_recursive(word[1:-1], low_index, high_index - 2)
