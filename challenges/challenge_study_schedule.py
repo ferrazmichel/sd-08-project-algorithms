34e717eee533bffa50b6604c4b44660fc0344643
Igor <igorln96@gmail.com>
2021-09-19 19:50:43 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count = 0
+
+    if not target_time:
+        return None
+
+    for student in permanence_period:
+        if not student[0] or not student[1]:
+            return None
+
+        if target_time >= student[0] and target_time <= student[1]:
+            count += 1
+
+    return count
