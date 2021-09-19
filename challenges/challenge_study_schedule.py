83c322492f04ffcf6d7bf4a3019b7cf5d8516068
Paulo Simões <paulohbsimoes@gmail.com>
2021-09-19 08:44:00 -0300

    feat: add req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    for start, end in permanence_period:
+        try:
+            if start <= target_time <= end:
+                counter += 1
+        except:
+            return None
+    return counter
