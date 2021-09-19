9db849ccf380ab0bf8893265fa044054225830f8
Paulo Simões <paulohbsimoes@gmail.com>
2021-09-19 14:27:45 -0300

    feat: add req 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,6 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    sorted_nums = sorted(nums)
+    for numA, numB in zip(sorted_nums, sorted_nums[1:]):
+        if numA == numB and min(numA, numB) > 0:
+            return numA
+    return False
