be9815c3ab92542b0959aff156df2310c9db3724
Paulo Simões <paulohbsimoes@gmail.com>
2021-09-19 09:19:22 -0300

    feat: add req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    return _is_palindrome(word, low_index, high_index)
+
+
+def _is_palindrome(word, low_index, high_index):
+    if high_index < low_index:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    return _is_palindrome(word, low_index + 1, high_index - 1)
