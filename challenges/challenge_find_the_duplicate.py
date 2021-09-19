f72a185b72603ef299244d4bf38884c2e1eff55a
Rita Jeveaux <ritajeveaux@gmail.com>
2021-09-19 10:33:03 -0300

    Req4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -2,9 +2,9 @@ def find_duplicate(nums):
     try:
         nums_sorted = sorted(nums)
         for i in range(len(nums_sorted) - 1):
-            if nums_sorted[i] < 0:
-                return False
-            elif nums_sorted[i] == nums_sorted[i + 1]:
+            # if nums_sorted[i] < 0:
+            #     return False
+            if nums_sorted[i] == nums_sorted[i + 1]:
                 return nums_sorted[i]
             else:
                 i += 1
