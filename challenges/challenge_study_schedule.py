b658db920f0bb394bc5ade57c1fd79d154a7665d
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-09-18 23:24:23 -0300

    Enviando requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    students_same_time = 0
+    try:
+        for student in permanence_period:
+            if student[0] <= target_time <= student[1]:
+                students_same_time += 1
+    except TypeError:
+        return None
+    return students_same_time
