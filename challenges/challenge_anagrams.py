2aab28c30a077bc9dd21952490954b3a3ba14c70
Paulo Simões <paulohbsimoes@gmail.com>
2021-09-19 13:37:25 -0300

    feat: add req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,45 @@
+from challenges.challenge_palindromes_recursive import is_palindrome_recursive
+
+
+def merge(stringA, stringB):
+    left_finger = 0
+    right_finger = 0
+    result = ""
+
+    while True:
+        current_a = stringA[left_finger]
+        current_b = stringB[right_finger]
+
+        if current_a <= current_b:
+            result += current_a
+            left_finger += 1
+        else:
+            result += current_b
+            right_finger += 1
+
+        if left_finger >= len(stringA):
+            result += stringB[right_finger:]
+            break
+
+        if right_finger >= len(stringB):
+            result += stringA[left_finger:]
+            break
+
+    return result
+
+
+def merge_sort(string):
+    if len(string) == 1:
+        return string
+
+    middle = len(string) // 2
+    substringA = string[:middle]
+    substringB = string[middle:]
+
+    return merge(merge_sort(substringA), merge_sort(substringB))
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+    return merge_sort(first_string) == merge_sort(second_string)
