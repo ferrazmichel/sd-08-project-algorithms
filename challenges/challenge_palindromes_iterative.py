9207c1f8cc7ee0f6eff4a906ef655a1746948f12
Paulo Simões <paulohbsimoes@gmail.com>
2021-09-19 14:43:33 -0300

    feat: add req 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,14 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+
+    left_index = 0
+    right_index = len(word) - 1
+
+    while left_index < right_index:
+        if word[left_index] != word[right_index]:
+            return False
+        left_index += 1
+        right_index -= 1
+
+    return True
