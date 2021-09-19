788451c20e839c72ecdc94b355626dca85ec6406
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-09-19 00:09:06 -0300

    Enviando requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,13 @@
+def letters_number(word):
+    letter_position = {}
+    for letter in word:
+        if letter in letter_position:
+            letter_position[letter] += 1
+        else:
+            letter_position[letter] = 1
+
+    return letter_position
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    return letters_number(first_string) == letters_number(second_string)
