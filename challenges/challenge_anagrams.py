4172e60f4d2ffb3d085d2fa05f73a4ea33889366
tandy <tandyfw@gmail.com>
2021-09-19 10:27:29 -0300

    Requisito 3 concluido 3/4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,17 @@
+def bubble_sort(array):
+    has_swapped = True
+    num_of_iterations = 0
+    while has_swapped:
+        has_swapped = False
+        for i in range(len(array) - num_of_iterations - 1):
+            if array[i] > array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+                has_swapped = True
+        num_of_iterations += 1
+    return array
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first = bubble_sort(first_string.split())
+    second = bubble_sort(second_string.split())
+    return "".join(first) == "".join(second)
