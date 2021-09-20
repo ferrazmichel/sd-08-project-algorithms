a4407813cb6c232c241d316d8941cf96535fb486
Ana Karine <anakarine.ti@gmail.com>
2021-09-20 01:33:17 -0300

    challenge_study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,22 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    """ Recebe:
+            - permanence_period:
+                * tupla que representa o período de permanência de uma pessoa
+                  estudante no sistema, com seu horário de entrada e saída;
+            - target_time:
+                * variável que representa os horários testados.
+        Retorna:
+            - o número de estudantes estudando no mesmo horário;
+            - None se em permanence_period houver alguma entrada inválida;
+            - None se target_time recebe um valor vazio.  """
+    count = 0
+    try:
+        for start, end in permanence_period:
+            if start <= target_time <= end:
+                count += 1
+        return count
+    except (TypeError, ValueError):
+        return None
+
+    """ Referência:
+        Plantão em 15/09/2021 com Bux """
