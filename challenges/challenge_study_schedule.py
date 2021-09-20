592d71f8e10554dd37b3f74c6435f4dd22369f3b
Wesley Paulo Ribeiro <wesley_paulo00@hotmail.com>
2021-09-20 06:03:44 -0300

    R01 - Concluido

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    students = 0
+    if (target_time) is None:
+        return None
+    for start_time in permanence_period:
+        if not isinstance(start_time[0], int) or not isinstance(
+            start_time[1], int
+        ):
+            return None
+        if start_time[0] <= target_time <= start_time[1]:
+            students += 1
+    return students
