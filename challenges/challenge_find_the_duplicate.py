5b42a37fcf1c4f28d451feea1fdae382f138647e
Rita Jeveaux <ritajeveaux@gmail.com>
2021-09-20 10:11:56 -0300

    Req4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,13 +1,16 @@
+# https://docs.python.org/pt-br/3/tutorial/errors.html
+# https://docs.python.org/pt-br/3/library/exceptions.html#AttributeError
+
 def find_duplicate(nums):
     try:
         nums_sorted = sorted(nums)
-        for i in range(len(nums_sorted) - 1):
+        for i in range(0, len(nums_sorted) - 1):
             # if nums_sorted[i] < 0:
             #     return False
-            if nums_sorted[i] == nums_sorted[i + 1]:
-                return nums_sorted[i]
-            else:
+            if nums_sorted[i] < 0 or nums_sorted[i] != nums_sorted[i + 1]:
                 i += 1
-    except TypeError:
+            else:
+                return nums_sorted[i]
+    except (TypeError, ValueError):
         return False
     return False
