a3169ea20a28889bd93d0c54938968ce4593448f
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-09-19 22:06:25 -0300

    exercise 5 - done

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,7 @@
+# solução parcial em:
+# https://www.geeksforgeeks.org/python-program-check-string-palindrome-not/
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    if not word:
+        return False
+    return word == word[::-1]
