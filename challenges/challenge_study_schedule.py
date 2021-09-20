d89cc3817cc06d3b4eab91d879c86968d91f112c
FIXsartoreto <fixsartoreto@gmail.com>
2021-09-20 10:05:42 -0300

    faltou os None

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,6 +1,9 @@
 def study_schedule(permanence_period, target_time):
     count = 0
     for person in permanence_period:
+        if not target_time or not person[0] or not person[1]:
+            return None
+
         if target_time >= person[0] and target_time <= person[1]:
             count += 1
     return count
