4e568e1e090b4ac95edca6c3adec747aa5886e78
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-09-19 21:52:14 -0300

    exercise 4 - done

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,12 @@
+# Solução parcial em:
+# https://www.javatpoint.com/python-program-to-print-the-duplicate-elements-of-an-array
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    result = None
+    for i in range(0, len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] == nums[j]:
+                result = nums[j]
+    if not result or result < 0:
+        return False
+    return result
