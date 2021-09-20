350291a4f16da9ef0a97006a5a12b7f282e69c0d
Aline Debastiani <debastianialine@gmail.com>
2021-09-20 18:51:33 -0300

    cumpre requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None:
+        return None
+    counter = 0
+
+    for start_time, end_time in permanence_period:
+        if type(start_time) != int or type(end_time) != int:
+            return None
+        if start_time <= target_time <= end_time:
+            counter += 1
+
+    return counter
