914238e2bce949d8641d9175cb0adb110e4d23a8
Anderson Santana <anderslash@gmail.com>
2021-09-21 18:36:09 -0300

    feat: retorna número de estudantes dado um target time

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    number_of_students = 0
+    for arrive_time, leave_time in permanence_period:
+        try:
+            if arrive_time <= target_time <= leave_time:
+                number_of_students += 1
+        except Exception:
+            return None
+    return number_of_students
