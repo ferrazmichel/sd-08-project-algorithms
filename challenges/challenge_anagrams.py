c4c6e6a966418ce3c8e270649967cd2585f07cbc
victor Farias <victorfarias.tv@gmail.com>
2021-09-21 13:42:39 -0300

    Requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,7 +1,21 @@
 def is_anagram(first_string, second_string):
-    if sorted(first_string) == sorted(second_string):
-        return True
-    if not first_string or not second_string:
-        return False
-    else:
-        return False
+    list_1 = [letter for letter in first_string]
+    list_2 = [letter for letter in second_string]
+
+    for index, letter in enumerate(list_1):
+        letter_index = None
+
+        for current_index in range(index, len(list_2)):
+            if letter == list_2[current_index]:
+                letter_index = list_2[current_index]
+                break
+
+        if not letter_index:
+            break
+
+        list_2[index], list_2[current_index] = (
+            list_2[current_index],
+            list_2[index],
+        )
+
+    return list_1 == list_2
