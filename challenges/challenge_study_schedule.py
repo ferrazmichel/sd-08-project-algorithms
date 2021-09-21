5bf56da155e779e17e9b23607fe41f70d1856372
Fernanda PC <nandateixmarques@gmail.com>
2021-09-20 22:22:28 -0300

    adicionando requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    CONTADOR = 0
+    try:
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                CONTADOR += 1
+    except TypeError:
+        return None
+    return CONTADOR
