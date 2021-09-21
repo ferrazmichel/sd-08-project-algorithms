fbd0acbf6adab9cf3f46c77690b1a625725af7f7
Rosiele David <rosieleserpa@hotmail.com>
2021-09-21 16:18:40 -0300

    Requisito 1 concluído

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    for start_time, end_time in permanence_period:
+        try:
+            if start_time <= target_time <= end_time:
+                counter += 1
+        except TypeError:
+            return None
+
+    return counter
