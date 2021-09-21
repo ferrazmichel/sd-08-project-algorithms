3ff3a21e545cb6e3ef49048a7ffbc2e74c1fb908
Victor Miwa <victoryukimiwa@hotmail.com>
2021-09-21 17:04:19 -0300

    Requisito 1 Estudantes busca teste de tempo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time:
+        return None
+    count = 0
+    for tuple in permanence_period:
+        if isinstance(tuple[0], int) and isinstance(tuple[1], int):
+            if tuple[0] <= target_time <= tuple[1]:
+                count += 1
+        else:
+            return None
+
+    return count
