36cbafd35fadfb889143f69a61546cbaf3713f40
Rosiele David <rosieleserpa@hotmail.com>
2021-09-21 18:33:01 -0300

    Requisito 4 concluído

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,40 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if not nums or type(nums) != list:
+        return False
+
+    # https://docs.python.org/pt-br/dev/howto/sorting.html
+    nums_sorted = sorted(nums)
+
+    found_number = None
+    for num in nums_sorted:
+        if type(num) != int or num <= 0:
+            return False
+
+        index = nums_sorted.index(num)
+
+        new_array = nums_sorted.copy()
+        new_array.pop(index)
+
+        found_number = search_num(new_array, 0, len(new_array) - 1, num)
+
+        if found_number:
+            return found_number
+
+    return found_number
+
+
+def search_num(array, low_index, high_index, num):
+    if high_index < low_index:
+        return False
+
+    middle_index = (low_index + high_index) // 2
+
+    if array[middle_index] == num:
+        return array[middle_index]
+    elif array[middle_index] > num:
+        return search_num(array, low_index, middle_index - 1, num)
+    else:
+        return search_num(array, middle_index + 1, high_index, num)
+
+
+print(find_duplicate([2, 3, 1]))
