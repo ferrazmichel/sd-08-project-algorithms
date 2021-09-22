dac68f272b8e907838e43aae4bef0f436533585b
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-09-22 19:31:11 -0300

    adicionando requisito 3 com insertion sort

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,5 +1,6 @@
 # UTILIZANDO BUBLE SORT NÃO PASSOU NO REQUISITO DO TEMPO
-def sort_words(array):
+# Source: Course Trybe
+def sort_words_with_buble(array):
     has_swapped = True
     num_of_iterations = 0
 
@@ -15,15 +16,41 @@ def sort_words(array):
     return array
 
 
+# UTILIZANDO O INSERTION SORT
+# Source: Course Trybe
+def sort_words_with_insertion_sort(array):
+    for i in range(len(array)):
+        current_value = array[i]
+        current_position = i
+        while (
+            current_position > 0
+            and array[current_position - 1] > current_value
+        ):
+            array[current_position] = array[current_position - 1]
+            current_position = current_position - 1
+        array[current_position] = current_value
+    return array
+
+
+# Dessa forma funciona e passa no requisito de tempo no entanto não utilizei
+# algoritmo de ordenação
+
 # def is_anagram(first_string, second_string):
+#     # caso as strings estejam vazias retorna False
 #     if len(first_string) == 0 or len(second_string) == 0:
 #         return False
 
+#     # Enquanto existam caracteres na first_string continua nesse bloco
 #     while len(first_string) > 0:
+#         # Inicia fazendo uma copia do primeiro caractere da first_string
 #         letter_to_take = first_string[0]
+#         # first_string vai receber ela mesmo sem os caracteres letter_to_take
 #         first_string = first_string.replace(letter_to_take, '')
+#         # second_string recebera ela mesmo sem os caracteres correspondentes
+#         # ao primeiro caractere da first_string
 #         second_string = second_string.replace(letter_to_take, '')
-
+#         # Caso as strings tenham um tamanho diferente significa
+#         # que tinham tamanhos diferentes
 #         if(len(first_string) != len(second_string)):
 #             return False
 
@@ -34,8 +61,12 @@ def is_anagram(first_string, second_string):
     if len(first_string) == 0 or len(second_string) == 0:
         return False
 
-    sorted_first_string = "".join(sort_words(list(first_string)))
-    sorted_second_string = "".join(sort_words(list(second_string)))
+    sorted_first_string = "".join(
+        sort_words_with_insertion_sort(list(first_string))
+    )
+    sorted_second_string = "".join(
+        sort_words_with_insertion_sort(list(second_string))
+    )
     if sorted_first_string != sorted_second_string:
         return False
     return True
