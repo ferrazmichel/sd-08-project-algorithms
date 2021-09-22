ee1c7208cd0bae7ba6b52bfb0957f21e6d587a31
Anderson Santana <anderslash@gmail.com>
2021-09-22 19:18:46 -0300

    feat: retorna se a palavra é um palíndromo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    """ Given a string, returns true if its a palindrome """
+    if len(word) == 0 or word[low_index] != word[high_index]:
+        return False
+    if len(word) < 2:
+        return True
+    return is_palindrome_recursive(word[1:high_index], low_index, -1)
