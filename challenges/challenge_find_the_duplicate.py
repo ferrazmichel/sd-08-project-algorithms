2b09c830f3e6693fe83d69b903c161b8044adba9
lucasbgama <lucasbg@protonmail.com>
2021-09-22 00:43:08 -0300

    requisito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,13 @@
+from challenges.challenge_anagrams import merge_sort
+
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if nums is None or len(nums) < 2:
+        return False
+    ordered_list = merge_sort(nums)
+    if(type(ordered_list[0]) == str or ordered_list[0] < 0):
+        return False
+    for num in range(len(ordered_list) - 1):
+        if(ordered_list[num] == ordered_list[num + 1]):
+            return ordered_list[num]
+    return False
