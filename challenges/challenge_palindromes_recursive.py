13a222a0bb1f8671f6b0945d1e53bfca6922e48b
enicio <jessandro42@gmail.com>
2021-09-22 18:12:59 -0300

    Palindrome recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,6 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    # print(len(word))
     if not word:
         return False
-    else:
-        return is_palindrome_recursive(word, low_index, high_index)
+    if low_index > high_index:
+        return True
+    elif word[low_index] != word[high_index]:
+        return False
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
