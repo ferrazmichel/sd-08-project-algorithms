4bf2f06aba2930ad4b56fd1ce36335175d786755
vitor <vitornunes200@gmail.com>
2021-09-22 15:25:46 -0300

    Req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,18 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui """
+
+    if not first_string or not second_string:
+        return False
+
+    first_array = list(first_string)
+    second_array = list(second_string)
+
+    for index in first_array:
+        if index in second_array:
+            second_array.remove(index)
+        else:
+            return False
+
+    if len(second_array) != 0:
+        return False
+    else:
+        return True
