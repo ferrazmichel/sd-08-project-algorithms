b533c866d04883be6a8bc69e6895a1f7701e29d9
lucasbgama <lucasbg@protonmail.com>
2021-09-22 00:16:04 -0300

    requisitos obrigatorios

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None:
+        return None
+    count = 0
+    for period in permanence_period:
+        if period[0] is None or period[1] is None:
+            return None
+        if period[0] <= target_time <= period[1]:
+            count += 1
+
+    return count
