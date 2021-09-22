82538bf64a83e09457940f1b3fa47660e0977859
Ana Higo <anahigo.arq@gmail.com>
2021-09-21 21:39:04 -0300

    Req. 2 Ok

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    """ Faça o código aqui.
+    https://stackoverflow.com/questions/30340208/recursively-checking-if-a-list-is-a-palindrome-in-python
+    """
+    if not word:
+        return False
+
+    if low_index == high_index or high_index < low_index:
+        return True
+
+    return (word[low_index] == word[high_index]
+            and is_palindrome_recursive(word, low_index + 1, high_index - 1))
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,18 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    """ Faça o código aqui.
+    - permanence_period - cadastra o horario de entrada e saída do aluno
+    cada tupla representa o período de permanência de uma pessoa estudante
+    com seu horario de entrada e saída
+    - Seu trabalho é descobrir qual o melhor horario para disponibilizar
+    os conteúdos
+    - Para achar o horário, será utilizada força bruta. Ou seja, para achar
+    o melhor horário, a função que você desenvolver será chamada várias vezes
+    com valores diferentes para a variável target_time, e serão analisados os
+    retornos da função.
+    - Dica: Quando vou saber qual o melhor horário? Quando o contador retornado
+    pela função for o maior.
+    - Dica: use um algoritmo de, no máximo, complexidade O(n)
+    - Algoritmo deve utilizar a solução iterativa;
+    - Caso o target_time passado seja nulo, o valor retornado pela função deve
+    ser None (considere o horário 0 como um horário válido);
+    """
