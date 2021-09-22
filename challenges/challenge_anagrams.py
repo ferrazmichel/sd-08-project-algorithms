272ed35149b8f858d48b8a538fc3bf36bb1ce334
enicio <jessandro42@gmail.com>
2021-09-22 19:26:42 -0300

    Anagrama

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,11 @@
+from challenges.insertion_sort import insertion_sort
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    sorted_first_string = insertion_sort(first_string)
+    sorted_second_string = insertion_sort(second_string)
+
+    if sorted_first_string == sorted_second_string:
+        return True
+    else:
+        return False
