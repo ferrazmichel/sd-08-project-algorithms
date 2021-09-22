03aaefcd99ea27c2310d52d96b15a6e4a0810b5c
Silvio Dayube <scarige@gmail.com>
2021-09-22 12:02:55 -0300

    Req(01): Brute force search

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    student_count = 0
+
+    try:
+        for student_hours in permanence_period:
+            if student_hours[0] <= target_time <= student_hours[1]:
+                student_count += 1
+    except TypeError:
+        return None
+
+    return student_count
