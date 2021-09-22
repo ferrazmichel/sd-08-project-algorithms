6ceb6a2269dea1e25081571026c47646d7b363b0
enicio <jessandro42@gmail.com>
2021-09-22 13:56:03 -0300

    Enicio do algorothms

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time:
+        return None
+    best_hour = 0
+    for begin, end in permanence_period:
+        if not (type(begin) == int and type(end) == int):
+            return None
+        if target_time >= begin and target_time <= end:
+            best_hour = best_hour + 1
+    return best_hour
