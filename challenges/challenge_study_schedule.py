0656d1d62be5c2bd8f672bdaa558f5e6b34ee824
amandaos <amanda.775.souza@gmail.com>
2021-09-22 19:49:27 -0300

    inicia projeto + Algoritmo de busca

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count = 0
+    try:
+        for student in permanence_period:
+            if student[0] <= target_time <= student[1]:
+                count += 1
+    except TypeError:
+        return None
+    return count
