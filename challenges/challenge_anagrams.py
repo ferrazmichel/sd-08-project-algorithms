384069b6e1b9ccdb5a21798ac7f2e8fd752c8b83
Anderson Santana <anderslash@gmail.com>
2021-09-22 20:29:16 -0300

    feat: retorna se as palavras são anagramas

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,11 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    """ Compare two strings and return if its an anagram """
+    if not first_string or not second_string:
+        return False
+    while len(first_string) != 0:
+        character = first_string[0]
+        first_string = first_string.replace(character, '')
+        second_string = second_string.replace(character, '')
+        if len(first_string) != len(second_string):
+            return False
+    return True
