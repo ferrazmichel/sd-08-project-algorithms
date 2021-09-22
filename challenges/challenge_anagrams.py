29d63752cfb3c96a899daded580ba32399438ce0
José Bernardo Johann <josebernardojohann@gmail.com>
2021-09-22 19:37:51 -0300

    Finishes project

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,26 @@
-def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+
+    mid = len(array) // 2
+
+    left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
+
+    return merge(left, right)
+
+
+def merge(left_array, right_array):
+    left_index = right_index = 0
+    left_length, right_length = len(left_array), len(right_array)
+
+    merged = []
+
+    while (left_index < left_length) and (right_index < right_length):
+        if left_array[left_index] <= right_array[right_index]:
+            merged.append(left_array[left_index])
+            left_index += 1
+        else:
+            merged.append(right_array[right_index])
+            right_index += 1
+
+    return merged + left_array[left_index:] + right_array[right_index:]
