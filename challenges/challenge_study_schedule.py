24a0cc3a317a5d310000b7ef2c3a74562d27ee5e
devfelipefranca <felipefrancafj@gmail.com>
2021-09-23 15:25:11 -0300

    FIX: First requirement

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,5 +1,6 @@
 def study_schedule(permanence_period, target_time):
-    if not target_time: return None
+    if not target_time: 
+        return None
     amount = 0
     for period in permanence_period:
         if target_time >= period[0] and target_time <= period[1]:
