5cf0e337e4c099bfac5f00fb5e6592adf4e2627e
devfelipefranca <felipefrancafj@gmail.com>
2021-09-23 17:43:25 -0300

    Update

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,7 +1,37 @@
-from merge import merge_sort
+def merge(stringA, stringB):
+    leftFinger = 0
+    rightFinger = 0
+    result = ""
+
+    while True:
+        currentStringA = stringA[leftFinger]
+        currentStringB = stringB[rightFinger]
+        if currentStringA <= currentStringB:
+            result += currentStringA
+            leftFinger += 1
+        else:
+            result += currentStringB
+            rightFinger += 1
+        if leftFinger >= len(stringA):
+            result += stringB[rightFinger:]
+            break
+        if rightFinger >= len(stringB):
+            result += stringA[leftFinger:]
+            break
+    return result
+
+
+def mergeSort(string):
+    if len(string) == 1:
+        return string
+
+    middle = len(string) // 2
+    stringA = string[:middle]
+    stringB = string[middle:]
+    return merge(mergeSort(stringA), mergeSort(stringB))
 
 
 def is_anagram(first_string, second_string):
     if len(first_string) != len(second_string):
         return False
-    return merge_sort(first_string) == merge_sort(second_string)
+    return mergeSort(first_string) == mergeSort(second_string)
