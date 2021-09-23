caae93c4d999d3083a44a8cb34a7bdff88e18819
devfelipefranca <felipefrancafj@gmail.com>
2021-09-23 17:06:55 -0300

    Update

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,10 +1,10 @@
 def is_anagram(first_string, second_string):
-    if not first_string:
+    if len(first_string) == 0:
         return False
-    if not second_string:
+    if len(second_string) == 0:
         return False
     firstStringList = list(first_string)
-    firstStringList.sort()
     secondStringList = list(second_string)
+    firstStringList.sort()
     secondStringList.sort()
     return (firstStringList == secondStringList)
