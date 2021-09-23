12f3961a4c17b735218732c39133d75d4c537b8b
Cleberton Francisco <clebertonfgc@gmail.com>
2021-09-23 07:43:00 -0300

    Finalizando solução requisito 03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -2,3 +2,9 @@ def is_anagram(first_string, second_string):
     reverse = "".join(list(second_string)[::-1])
     if len(first_string) != len(second_string):
         return False
+    if first_string == second_string:
+        return True
+    if first_string != reverse:
+        return True
+    else:
+        return False
