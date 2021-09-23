465bb0a3d2e9cc64c8c08f85a5df5211ee304f17
Cleberton Francisco <clebertonfgc@gmail.com>
2021-09-23 08:10:38 -0300

    Requisito 04

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,8 @@
-def find_duplicate(nums):
-    """ Faça o código aqui. """
+def find_duplicate(numbers):
+    if len(numbers) <= 1 or not numbers:
+        return False
+    for number in numbers:
+        if type(number) != int or number < 0:
+            return False
+    num = max(numbers, key=numbers.count)
+    return False if numbers.count(num) <= 1 else num
