6d351f5a3339d0193587c735eec96c43f2c202ca
devfelipefranca <felipefrancafj@gmail.com>
2021-09-23 17:00:30 -0300

    Update

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,4 +1,8 @@
 def is_anagram(first_string, second_string):
+    if not first_string:
+        return False
+    if not second_string:
+        return False
     firstStringList = list(first_string)
     firstStringList.sort()
     secondStringList = list(second_string)
