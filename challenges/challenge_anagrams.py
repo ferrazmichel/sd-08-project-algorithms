ad21472c0ef7c7c8ce6393ec3996e0d1bab24af1
gabrielDonnantuoni <gabrieldonnantuoni@gmail.com>
2021-09-22 22:45:12 -0300

    feat: Add is_anagram implementation

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,9 @@
+from merge_sort import merge_sort
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    '''
+    Returns True if the two strings are anagrams,
+    False otherwise.
+    '''
+    return merge_sort(first_string) == merge_sort(second_string)
