01f0a9cb509cf5788a6eccecc9347d9c13338330
devfelipefranca <felipefrancafj@gmail.com>
2021-09-23 18:00:17 -0300

    Update

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,4 +1,8 @@
 def is_palindrome_iterative(word):
+    lowIndex = 0
+    highIndex = len(word) - 1
+    if word[lowIndex] != word[highIndex]:
+            return False
     wordLength = len(word)
     reversedWord = word[wordLength::-1]
     return word == reversedWord
