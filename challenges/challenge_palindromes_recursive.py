e32f85ea983b066a4f59cfb807c59ce0e27080cf
gabrielDonnantuoni <gabrieldonnantuoni@gmail.com>
2021-09-22 21:23:11 -0300

    feat: Add is_palindrome_recursive implementation

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    '''
+    Receives a string, start index and end index
+
+    Returns True if the string is a palindrome and False if
+    it is empty or not a palindrome.
+    '''
+    if not word or word[low_index] != word[high_index]:
+        return False
+    elif low_index >= high_index:
+        return True
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
