31d047e06106d10a79007cfcfcad7d38c69b8e98
devfelipefranca <felipefrancafj@gmail.com>
2021-09-23 15:21:08 -0300

    First requiriment

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,7 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time: return None
+    amount = 0
+    for period in permanence_period:
+        if target_time >= period[0] and target_time <= period[1]:
+            amount += 1
+    return amount
