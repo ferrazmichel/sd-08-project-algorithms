4be53b16ccf547e715c68d2679872ae1dff27cdf
Cleberton Francisco <clebertonfgc@gmail.com>
2021-09-22 22:45:15 -0300

    iniciando solução

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,4 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    reverse = "".join(list(second_string)[::-1])
+    if len(first_string) != len(second_string):
+        return False
