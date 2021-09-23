06917c6acc6bacac52c8028830971955d50d3656
gabrielDonnantuoni <gabrieldonnantuoni@gmail.com>
2021-09-22 23:15:19 -0300

    feat: Add find_duplicate implementation

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,17 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    '''
+    Returns the duplicate number if exists, False
+    otherwise.
+    '''
+    if (
+        not isinstance(nums, list)
+        or len(nums) < 2
+        or any((not isinstance(num, int) or num < 0) for num in nums)
+    ):
+        return False
+    sorted_nums = sorted(nums)
+
+    for index, num in enumerate(sorted_nums[:-1]):
+        if num == sorted_nums[index + 1]:
+            return num
+    return False
