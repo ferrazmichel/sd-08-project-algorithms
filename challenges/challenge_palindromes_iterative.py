fa1008b1a7d045e3b2517866ed4c15b0a71949e5
gabrielDonnantuoni <gabrieldonnantuoni@gmail.com>
2021-09-22 21:11:28 -0300

    feat: Add is_palindrome_iterative implementation

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,18 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    '''
+    Receives a string, start index and end index
+
+    Returns True if the string is a palindrome and False if
+    it is empty or not a palindrome.
+    '''
+    if not word:
+        return False
+    low_index = 0
+    high_index = len(word) - 1
+
+    while low_index < high_index:
+        if word[low_index] != word[high_index]:
+            return False
+        low_index += 1
+        high_index -= 1
+    return True
