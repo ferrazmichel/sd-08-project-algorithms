65b382f1223abbadd79fb05f7d7ad5336554e2ec
Cleberton Francisco <clebertonfgc@gmail.com>
2021-09-22 22:41:01 -0300

    Requisito 02

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    list_word = list(word)
+
+    if len(word) < 1:
+        return False
+    if low_index == high_index:
+        return True
+    if list_word[high_index] != list_word[low_index]:
+        return False
+    return is_palindrome_recursive(list_word, low_index + 1, high_index - 1)
