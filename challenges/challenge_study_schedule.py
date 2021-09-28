ae4073af7210f2757ff3e3b56b5d39649337c8aa
Clênio Galvão <clenio.cgm@gmail.com>
2021-09-27 21:08:32 -0300

    Refatorando quesito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,16 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """Função para identificar o horário com maior tráfego no site da escola"""
-    if target_time is None:
-        return None
-
     contador = 0
+    try:
+        for permanence in permanence_period:
+            if permanence[0] <= target_time <= permanence[1]:
+                contador += 1
 
-    for permanence in permanence_period:
-        if permanence[0] is None or permanence[1] is None or \
-          type(permanence[0]) != int or type(permanence[1]) != int:
-            return None
-
-        if permanence[0] <= target_time <= permanence[1]:
-            contador += 1
-
-    return contador
+        return contador
+    except TypeError:
+        return None
