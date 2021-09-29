2acae453091e1b6762fe040a7e2f1a601982a087
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-09-29 15:27:08 -0300

    is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,38 @@
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+
+    mid = len(array) // 2
+    left = merge_sort(array[:mid])
+    right = merge_sort(array[mid:])
+    # Notar que pra execução da próxima linha
+    # é considerado o array anterior a criação do left e right,
+    # portanto será executado este return.
+    # Na dúvida usar debug com uma string de dois caracteres)
+    return merge(left, right, array.copy())
+
+
+def merge(left, right, merged):
+
+    left_cursor, right_cursor = 0, 0
+    while left_cursor < len(left) and right_cursor < len(right):
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first_array = list(first_string)
+    second_array = list(second_string)
+    return merge_sort(first_array) == merge_sort(second_array)
