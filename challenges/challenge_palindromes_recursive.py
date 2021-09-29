6edd1e7467a78ad99a059dc0a203bc4abca1fdee
Clênio Galvão <clenio.cgm@gmail.com>
2021-09-29 07:18:59 -0300

    Inicia quesito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if word == '':
+        return False
+
+    if len(word) > 1:
+        if word[0] != word[-1]:
+            return False
+
+        else:
+            is_palindrome_recursive(word[1:high_index], 0, high_index - 1)
+
+    else:
+        return True
