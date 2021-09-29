9adb793713874dfd7ffcdf9450b332c2c536afd6
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-09-29 08:06:16 -0300

    study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    sum = 0
+    if (not target_time):
+        return None
+    else:
+        for period in permanence_period:
+            if type(period[0]) != int or type(period[1]) != int:
+                return None
+            if period[0] <= target_time <= period[1]:
+                sum += 1
+
+        return sum
