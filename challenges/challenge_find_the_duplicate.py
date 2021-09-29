af0c1fe1bd739e85e38b2d67cfc24894566449dd
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-09-29 16:20:17 -0300

    find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,9 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    nums.sort()
+    result = False
+    for num, one_ahead in zip(nums, nums[1:]):
+        if type(num) == int and num < 0:
+            return False
+        if num == one_ahead:
+            return num
+    return result
