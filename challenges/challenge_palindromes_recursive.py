58e2fabbeff804664f7aafba523b1a4a3bebd823
Clênio <clenio.martins@activesoftconsultoria.com.br>
2021-10-01 13:27:14 -0300

    Corrige erros do flake

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,12 +2,12 @@ def is_palindrome_recursive(word, low_index, high_index):
     """Função recursiva para verificar se uma palavra é palindrome."""
     if word == '':
         return False
-      
+
     if (high_index - low_index) <= 2:
-      if word[low_index] == word[high_index]:
-        return True
-      else:
-        return False
+        if word[low_index] == word[high_index]:
+            return True
+        else:
+            return False
 
     else:
-      return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
