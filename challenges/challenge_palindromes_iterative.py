41d905454a131976373bd03c1c46adb710470bcc
Clênio Galvão <clenio.cgm@gmail.com>
2021-10-02 14:35:58 -0300

    Resolve quesito 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,12 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    """Função iterativa para verificar se uma palavra é palindrome."""
+    if word == '':
+        return False
+
+    for posicao in range(len(word) // 2):
+        if word[posicao] != word[-1]:
+            return False
+        else:
+            word = word[:len(word) - 1]
+
+    return True
