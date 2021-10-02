efb2010f0fc7a9a3821f23df32edf8d37acb8009
Clênio Galvão <clenio.cgm@gmail.com>
2021-10-02 12:57:33 -0300

    Resolve quesito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,19 @@
+def search(array, value):
+    for element in array:
+        if element == value:
+            return value
+
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    if not nums:
+        return False
+
+    ordenado = sorted(nums)
+    for idx, num in enumerate(ordenado):
+        if not isinstance(num, int) or num < 0:
+            return False
+        repetido = search(ordenado[idx + 1:], num)
+        if repetido:
+            return repetido
+
+    return False
