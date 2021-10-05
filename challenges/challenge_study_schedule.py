e5b078a33f17d7f9ec345e5632456d836e82a6ee
Roberto Asgon <robertoasgon@gmail.com>
2021-10-05 15:40:25 -0300

    feat: first requirement

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    students_qntty = 0
+
+    for start_time, end_time in permanence_period:
+        try:
+            if target_time >= start_time and target_time <= end_time:
+                students_qntty += 1
+        except Exception:
+            return None
+    return students_qntty
