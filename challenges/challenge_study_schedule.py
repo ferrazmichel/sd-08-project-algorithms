05e7135deabc255fe9c65b7ccf6703ad534c5f3d
Daniel <marcio.daniel@msn.com>
2021-10-05 15:56:46 -0300

    Req 1 Done

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. First commmit"""
+    count = 0
+    try:
+        for cur in permanence_period:
+            if(cur[0]) <= target_time <= cur[1]:
+                count += 1
+        return count
+    except TypeError:
+        return None
